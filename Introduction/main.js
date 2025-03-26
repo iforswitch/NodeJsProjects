ce9566dc400ed8a4f63a6d77b8138a7f7732b617
iforswitch <switchorif@gmail.com>
2025-03-26 10:27:46 +0000

    feat: First electron window program done, from tutorial

---
+++ Introduction/main.js
@@ -0,0 +1,15 @@
+console.log("Hello from Electron 👋")
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 400,
+    height: 400
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
